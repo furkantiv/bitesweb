9ee2979e0a973280710b1e8040d625c4c83a54e8
furkantiv <eiusbey@gmail.com>
2025-06-17 15:15:51 +0300

    fix: Update next.config.ts to include basePath and assetPrefix configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV;
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/bitesweb",
+  assetPrefix: "/bitesweb/",
   images: {
     unoptimized: true,
   },
