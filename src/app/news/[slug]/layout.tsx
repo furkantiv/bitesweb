f12f1634a85eda1492e4c797b2da658e48b189d1
furkantiv <eiusbey@gmail.com>
2025-07-08 15:41:09 +0300

    refactor: Simplify NewsDetailLayout by removing unused params and cleaning up the structure

--- src/app/news/[slug]/layout.tsx
+++ src/app/news/[slug]/layout.tsx
@@ -1,19 +1,14 @@
 import BreadcrumbsWithSearch from "@/components/ui/BreadcrumbsWithSearch";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export default function NewsDetailLayout({
   children,
-  params,
 }: {
   children: ReactNode;
-  params: { slug: string };
 }) {
-  const title = decodeURIComponent(params.slug.replace(/-/g, " "));
   return (
-    <div className="max-w-7xl mx-auto px-6 py-20 md:px-0 md:pt-20 w-full">
-      <BreadcrumbsWithSearch
-        items={[{ label: "News", href: "/news" }, { label: title }]}
-      />
+    <div>
+      {/* Breadcrumb veya diğer ortak bileşenler */}
       {children}
     </div>
   );
