1f55e817aa39ae15149f60f0b3b834a4d6f5aa43
furkantiv <eiusbey@gmail.com>
2025-07-17 10:40:42 +0300

    refactor address

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -2,12 +2,11 @@
 
 import React, { CSSProperties, useState } from "react";
 import Image from "next/image";
-import { ClipLoader, MoonLoader } from "react-spinners";
+import { MoonLoader } from "react-spinners";
 
 const override: CSSProperties = {
-  display: "block",
   margin: "0 auto",
-  borderColor: "red",
+  borderColor: "white",
 };
 const LoadingSpinner = () => {
   let [loading, setLoading] = useState(true);
@@ -24,38 +23,17 @@ const LoadingSpinner = () => {
         data-testid="loader"
       />
       {/* Logo ortada */}
-      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+      <div className="absolute inset-0 flex items-center justify-center bg-transparent pointer-events-none">
         <Image
-          src="/inner-logo.svg" // ← burayı kendi logonla değiştir
+          src="/inner-logo.svg"
           alt="Logo"
-          width={64} // Boyutunu isteğine göre ayarla
+          width={64}
           height={64}
           className="rounded-full"
           priority
         />
       </div>
     </div>
-
-    // <div className="min-h-screen w-full bg-[#000816] flex items-center justify-center">
-    //   <div className="text-center">
-    //     <div className="relative w-64 h-64 mx-auto mb-4">
-    //       {/* Dönen çember */}
-    //       <div className="absolute inset-0 rounded-full border-16 border-blue-400 border-t-transparent animate-spin" />
-    //       {/* Ortadaki logo */}
-    //       <div className="absolute inset-2 flex items-center justify-center">
-    //         <Image
-    //           src="/inner-logo.svg" // ← burada kendi logonu kullanabilirsin
-    //           alt="Logo"
-    //           width={256}
-    //           height={256}
-    //           className="rounded-full"
-    //           priority
-    //         />
-    //       </div>
-    //     </div>
-    //     <div className="text-white font-light tracking-wider">BITES</div>
-    //   </div>
-    // </div>
   );
 };
 
